b6fd92bc64f4a6e073198a926c42538149b481ce
Paula Wiltiner <paulawiltiner.pw@gmail.com>
2022-04-16 15:26:10 -0300

    participantes ativos

--- script.js
+++ script.js
@@ -3,23 +3,111 @@ let apiDriven = "https://mock-api.driven.com.br/api/v6/uol/";
 let nome_entrada;
 let menssagem;
 let chat =  document.querySelector(".chat");
+let to="";
+let tipo_msg="";
+let aviso_envio="";
+let tipo;
 
-function contatos () {
-    
+function aviso() {
+    aviso_envio = "Enviando para " + to;
+    if(tipo_msg==="Reservadamente"){
+        aviso_envio = "Enviando para " + to + " (reservadamente)"
+    }
+    if(tipo_msg==="Público"){
+        aviso_envio = "Enviando para " + to + " (público)"
+    }
+    let aviso = document.querySelector(".aviso");
+    aviso.innerHTML = aviso_envio;
+    aviso.classList.add("enviando");
+}
+
+function abrir_menu(){
+    aviso()
+    let fundo = document.querySelector(".fundo");
+    fundo.classList.add("fundo_menu");
+    fundo.setAttribute("onclick","ocultar()")
+    let menu_lateral = document.querySelector(".menu");
+    menu_lateral.classList.add("menu_lateral");
+    menu_lateral.innerHTML=" <div class='contatos'><div class='titulo_contatos'>Escolha um contato para enviar mensagem:</div><div class='contato' onclick='check_contato(this)'><div class='parteum'><ion-icon class='icone_people_contato' name='people'></ion-icon><h2>Todos</h2></div><img class='img'/></div><div class='contato' onclick='check_contato(this)'><div class='parteum'><ion-icon class='icone_people_contato' name='person-circle'></ion-icon><h2>João</h2></div><img class='img'/></div></div><div class='visibilidade'><div class='titulo_visibilidade'>Escolha a visibilidade:</div><div class='contato' onclick='check_visibilidade(this)'><div class='parteum'><ion-icon class='icone_lock' name='lock-open'></ion-icon><h2>Público</h2></div><img class='img'/></div><div class='contato' onclick='check_visibilidade(this)'><div class='parteum'><ion-icon class='icone_lock' name='lock-closed'></ion-icon><h2>Reservadamente</h2></div><img class='img'/></div></div>";
+}
+
+
+function ocultar () {
+    if (to==="Todos" && tipo_msg==="Reservadamente"){
+        alert("Para que a mensagem seja enviada para Todos , por favor, escolha a opção Público.")
+    }else {
+        if(to === "" || tipo_msg ==="" ){
+           alert("Antes de enviar a sua menssagem , você precisa escolher o destinatário e o tipo de visibilidade.")
+        } else {
+            let menu_lateral = document.querySelector(".menu");
+            menu_lateral.innerHTML="";
+            menu_lateral.classList.remove("menu_lateral");
+            let fundo = document.querySelector(".fundo");
+            fundo.classList.remove("fundo_menu");
+        }
+    }
+}
+
+function check_contato (elemento) {
+    let contatos = document.querySelector(".contatos");
+    if(contatos.querySelector(".check")===null){
+        elemento.querySelector("img").setAttribute("src","imagens/checkmark.png");
+        elemento.classList.add("check");
+        to=elemento.querySelector(".parteum").querySelector("h2").innerHTML;
+        aviso()
+    }else {
+        if(elemento.classList.contains("check")===false){
+            contatos.querySelector(".check").querySelector("img").removeAttribute("src");
+            contatos.querySelector(".check").classList.remove("check");
+            elemento.querySelector("img").setAttribute("src","imagens/checkmark.png")
+            elemento.classList.add("check");
+            to=elemento.querySelector(".parteum").querySelector("h2").innerHTML;
+            aviso()
+        }
+    }
+}
+
+function check_visibilidade (elemento) {
+    let contatos = document.querySelector(".visibilidade");
+    if(contatos.querySelector(".check")===null){
+        elemento.querySelector("img").setAttribute("src","imagens/checkmark.png");
+        elemento.classList.add("check");
+        tipo_msg=elemento.querySelector(".parteum").querySelector("h2").innerHTML;
+        aviso()
+    }else {
+        if(elemento.classList.contains("check")===false){
+            contatos.querySelector(".check").querySelector("img").removeAttribute("src");
+            contatos.querySelector(".check").classList.remove("check");
+            elemento.querySelector("img").setAttribute("src","imagens/checkmark.png")
+            elemento.classList.add("check");
+            tipo_msg=elemento.querySelector(".parteum").querySelector("h2").innerHTML;
+            aviso()
+        }
+    }
 }
 
 function envio () {
     menssagem = document.querySelector(".texto").value ;
-    let dados = {
-        from: nome_entrada,
-        to: "Todos",
-        text: menssagem,
-        type: "message" 
+    if(to==="" || tipo_msg===""){
+        alert ("Antes de enviar , você precisa escolher o destinatário.")
+    } else {
+        if(tipo_msg==="Reservadamente"){
+            tipo = "private_message"
+        }
+        if(tipo_msg==="Público"){
+            tipo = "message"
+        }
+        let dados = {
+            from: nome_entrada,
+            to: to,
+            text: menssagem,
+            type: tipo
+        }
+        let promise = axios.post(apiDriven+"messages", dados);
+        promise.then(buscar_msg);
+        promise.catch(erroEnvio);
+        document.querySelector("input").value="" ;
     }
-    let promise = axios.post(apiDriven+"messages", dados);
-    promise.then(buscar_msg)
-    promise.catch(erroEnvio)
-    document.querySelector("input").value="" ;
 }
 
 function erroEnvio() {
